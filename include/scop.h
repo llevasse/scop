2bbf6939177d64a1a61692020bb33e37d04c1dae
llevasse <elise.levasse@gmail.com>
2024-12-11 17:05:49 +0100

    init more structs

--- include/scop.h
+++ include/scop.h
@@ -10,14 +10,30 @@
 int open_obj(char *path);
 
 typedef struct s_obj{
+	int						material_fd;
+	char					*material_path;
 	t_material **material_list;
 }	t_obj;
 
 typedef struct s_material{
-	int						material_fd;
-	char					*material_path;
+	char					*name;
+	float 					ambient_color;	//said 'ka'
+	float					diffuse_color;	//said 'kd'
+	float					specular_color;	//said 'ks'
+	float					shininess;		//said 'ns'
+	float					dissolve;		//said 'd'
+	
 	t_vertices				**vertices;
-	t_verctex_normals		**vertex_normals
-	t_texture_coordinates	**vertex_tertures
+	t_vertex_normals		**vertex_normals;
+	t_texture_coordinates	**vertex_tertures;
 }	t_material;
+
+typedef struct s_vertices{
+} t_vertices;
+
+typedef struct s_vertex_normals{
+} t_vertex_normals;
+
+typedef struct s_texture_coordinates{
+} t_texture_coordinates;
 #endif
