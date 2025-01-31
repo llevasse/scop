8fb21593bd7aaa5d01ef7153a5a4adde52bbe8fe
llevasse <elise.levasse@gmail.com>
2025-01-31 12:10:48 +0100

    [parse] concave face as single face

--- srcs/parse_faces.c
+++ srcs/parse_faces.c
@@ -5,6 +5,7 @@ extern struct s_garbage	*g_garbage_collector;
 
 void	add_triangle(char **tab, t_scene *scene, int line_nb);
 void	add_quad(char **tab, t_scene *scene, int line_nb);
+void	add_mesh(char **tab, size_t tab_size, t_scene *scene, int line_nb);
 
 t_faces	*face_contructor(){
 	t_faces *face = malloc(sizeof(struct s_faces));
@@ -29,9 +30,10 @@ void	parse_face(char **tab, size_t tab_size, t_scene *scene, int line_nb){
 	else if (tab_size == 5)
 		add_quad(tab, scene, line_nb);
 	else{
-		triangulate(tab, tab_size, scene, line_nb);
-		dprintf(2,"Face on line %d has too many vertex :(\n", line_nb);
-		free_garbage();
+		add_mesh(tab, tab_size, scene, line_nb);
+		//triangulate(tab, tab_size, scene, line_nb);
+		// dprintf(2,"Face on line %d has too many vertex :(\n", line_nb);
+		// free_garbage();
 	}
 }
 
@@ -205,5 +207,104 @@ void	add_quad(char **tab, t_scene *scene, int line_nb){
 			tmp = tmp->next;
 		}
 		tmp->next = f1;
+	}
+}
+
+
+void	add_mesh(char **tab, size_t tab_size, t_scene *scene, int line_nb){
+	t_faces	*faces[tab_size - 1];
+	for (size_t i = 0 ; i < tab_size - 1; i++){
+		faces[i] = face_contructor();
+		faces[i]->material = scene->objs_list->material;
+	}
+
+	scene->objs_list->material->nb_faces += tab_size - 1;
+	scene->display_vertices_count += (tab_size - 1) * 3;
+
+
+	t_vertices *vs[tab_size - 1];
+	t_texture_coordinates *vts[tab_size - 1];
+	t_vertices *vns[tab_size - 1];
+	for (size_t i = 0; i < tab_size - 1; i++){
+		vts[i] = 0x0;
+		vns[i] = 0x0;
+	}
+	size_t index = 0, texture_index = 0, vertex_normal_index = 0;
+	for (size_t i =0; i < tab_size - 1; i++){
+		index = atoi(tab[i + 1]);
+		if (index > scene->vertices_count){
+			dprintf(2,"Face on line %d call un declared vertex :(\n", line_nb);
+			free_garbage();
+		}
+		vs[i] = scene->vertices_tab[index - 1];
+
+		if (tab[i+1][get_int_len(index)] == '/'){
+			texture_index = atoi(tab[i+1] + get_int_len(index) + 1);
+			if (texture_index == 0){
+				vts[i] = 0x0;
+				if (tab[i+1][get_int_len(index)+1] == '/'){
+					vertex_normal_index = atoi(tab[i+1] + get_int_len(index) + 2);
+					if (vertex_normal_index > scene->vertex_normals_count){
+						dprintf(2,"Face on line %d call un declared vertex normal :(\n", line_nb);
+						free_garbage();
+					}
+					vns[i] = scene->vertex_normals_tab[vertex_normal_index - 1];
+				}
+			}
+			else {
+				if (texture_index > scene->texture_coordinates_count){
+					dprintf(2,"Face on line %d call un declared texture coordinate :(\n", line_nb);
+					free_garbage();
+				}
+				vts[i] = scene->texture_coordinates_tab[texture_index - 1];
+				if (tab[i + 1][get_int_len(index) + 1 + get_int_len(texture_index)] == '/'){
+					vertex_normal_index = atoi(tab[i+1] + get_int_len(index) + get_int_len(texture_index) + 2);
+					if (vertex_normal_index > scene->vertex_normals_count){
+						dprintf(2,"Face on line %d call un declared vertex normal :(\n", line_nb);
+						free_garbage();
+					}
+					vns[i] = scene->vertex_normals_tab[vertex_normal_index - 1];
+				}
+			}
+		}
+	}
+
+	size_t v_idx0 = vs[tab_size - 2]->id, v_idx1 = 0, v_idx2 = 1;
+	for (size_t i = 0; i < tab_size - 1; i++, v_idx1++){
+		if (v_idx0 >= tab_size)
+			break ;
+		v_idx0 = v_idx1 - 1;
+		v_idx2 = v_idx1 + 1;
+		if (v_idx1 - 1 > tab_size){
+			v_idx0 = tab_size - 2;
+		}
+		if (v_idx1 + 1 > tab_size - 2){
+			v_idx2 = 0;
+		}
+		faces[i]->vertices[0] = vs[v_idx0];
+		faces[i]->vertices[1] = vs[v_idx1];
+		faces[i]->vertices[2] = vs[v_idx2];
+		printf("new face : %zu %zu %zu\n", vs[v_idx0]->id, vs[v_idx1]->id, vs[v_idx2]->id);
+	}
+	t_faces *tmp = 0x0;
+	if (!scene->objs_list->faces){
+		scene->objs_list->faces = faces[0];
+	}
+	else{
+		tmp = scene->objs_list->faces;
+		while (tmp->next){
+			tmp = tmp->next;
+		}
+	}
+	for (size_t i = 0; i< tab_size - 1; i++){
+		map_face_uv(faces[i]);
+		printf("face %zu %zu %zu normal : %f %f %f\n", faces[i]->vertices[0]->id, faces[i]->vertices[1]->id, faces[i]->vertices[2]->id, faces[i]->normal.x, faces[i]->normal.y, faces[i]->normal.z);
+		if (!tmp){
+			tmp = faces[0];
+		}
+		else{
+			tmp->next = faces[i];
+			tmp = tmp->next;
+		}
 	}
 }
--- srcs/triangulate.c
+++ srcs/triangulate.c
@@ -9,15 +9,91 @@ extern struct s_garbage	*g_garbage_collector;
 // P = ( -Ey, Ex )
 // h = ( (A-C) * P ) / ( F * P )
 
-short	check_if_segment_cross(t_vertices *v1, t_vertices *v2, t_vertices *v3, t_vertices *v4){
-	t_vertices E = subtract_vectors(v2, v1);
-	t_vertices F = subtract_vectors(v4, v3);
-	t_vertices G = subtract_vectors(v3, v1);
+float	check_if_segment_cross(t_vertices v1, t_vertices v2, t_vertices v3, t_vertices v4){
+	
+	t_vertices	d1 = subtract_vectors(&v2, &v1);
+	t_vertices	d2 = subtract_vectors(&v4, &v3);
+
+	t_vertices E = vector_cross_product(&d1, &d2);
+	t_vertices F = subtract_vectors(&d1, &d2);
+
+	return (vector_dot_product(&E, &F));
+
+
+
+	/*v1.x /= v1.z;
+	v1.y /= v1.z;
+
+	v2.x /= v2.z;
+	v2.y /= v2.z;
+
+	v3.x /= v3.z;
+	v3.y /= v3.z;
+
+	v4.x /= v4.z;
+	v4.y /= v4.z;*/
+	
+	// t_vertices E = subtract_matrixed_vectors(&v2, &v1);
+	// t_vertices F = subtract_matrixed_vectors(&v4, &v3);
+	// t_vertices G = subtract_matrixed_vectors(&v3, &v1);
+	/*
+	t_vertices E = subtract_vectors(&v2, &v1);
+	t_vertices F = subtract_vectors(&v4, &v3);
+	t_vertices G = subtract_vectors(&v3, &v1);
+
 	t_vertices P = E;
 	P.y = -P.y;
 	float	h = (vector_dot_product(&G, &P) / vector_dot_product(&F, &P));
-	return (h >= 0 && h <= 1);
+	return (h);*/
 }
+
+
+// project to same plane
+void	project_to_coplanarity(char **tab, size_t tab_size, t_scene *scene){
+	t_vertices	*v1, *v2, *v3;
+	size_t		index[tab_size - 1];
+	for (size_t i = 1; i < tab_size; i++){
+		index[i - 1] = atoi(tab[i]) - 1;
+	}
+	v1 = scene->vertices_tab[index[0]];
+	v2 = scene->vertices_tab[index[1]];
+	v3 = scene->vertices_tab[index[2]];
+
+	v1->matrixed_x = v1->x;
+	v1->matrixed_y = v1->y;
+	v1->matrixed_z = v1->z;
+
+	v2->matrixed_x = v2->x;
+	v2->matrixed_y = v2->y;
+	v2->matrixed_z = v2->z;
+
+	v3->matrixed_x = v3->x;
+	v3->matrixed_y = v3->y;
+	v3->matrixed_z = v3->z;
+	t_vertices	U = subtract_vectors(v2, v1), V = subtract_vectors(v3, v1); 
+
+	t_vertices vn = vector_cross_product(&U, &V);
+	normalize_vector(&vn);
+
+
+	// project vectors onto the same plane
+	for (size_t i = 3; i < tab_size - 1; i++){
+		t_vertices tmp_vn = vn;
+		t_vertices tmp = subtract_vectors(scene->vertices_tab[index[i]], v1);
+		float scalar = vector_dot_product(&tmp, &tmp_vn);
+		tmp_vn.x *= scalar;
+		tmp_vn.y *= scalar;
+		tmp_vn.z *= scalar;
+
+		tmp = subtract_vectors(&tmp, &tmp_vn);
+		scene->vertices_tab[index[i]]->matrixed_x = tmp.x;
+		scene->vertices_tab[index[i]]->matrixed_y = tmp.y;
+		scene->vertices_tab[index[i]]->matrixed_z = tmp.z;
+	}
+	
+
+}
+
 
 void	triangulate(char **tab, size_t tab_size, t_scene *scene, int line_nb){
 	short	crossed[tab_size - 1][tab_size - 1];
@@ -27,9 +103,10 @@ void	triangulate(char **tab, size_t tab_size, t_scene *scene, int line_nb){
 		for (size_t j = 0; j < tab_size; j++){
 			crossed[i - 1][j] = 0;
 		}
-		index[i - 1] = atoi(tab[i]);
+		index[i - 1] = atoi(tab[i]) - 1;
 	}
 
+	//project_to_coplanarity(tab, tab_size, scene);
 
 	for (size_t i = 0; i < tab_size - 1; i++){
 		for (size_t j = 0; j < tab_size - 1; j++){
@@ -42,12 +119,22 @@ void	triangulate(char **tab, size_t tab_size, t_scene *scene, int line_nb){
 					continue;
 				v3 = scene->vertices_tab[index[l]];
 				size_t k = l + 1;
-				if (k >= tab_size)
+				if (k >= tab_size - 1)
 					k = 0;
 				if (k == l || k == i || k == j)
 					continue;
 				v4 = scene->vertices_tab[index[k]];
-				crossed[i][j] = check_if_segment_cross(v1, v2, v3, v4);
+
+				
+
+				float	value = check_if_segment_cross(*v1, *v2, *v3, *v4);
+				printf("check %zu %zu cross %zu %zu (%f)\n", v1->id, v2->id, v3->id, v4->id, value);
+				//float	value2 = check_if_segment_cross(*v3, *v4, *v1, *v2);
+				if ((value > 0 && value < 1)){
+					printf("\t %zu %zu cross %zu %zu (%f)\n", v1->id, v2->id, v3->id, v4->id, value);
+					crossed[i][j] = 1;
+					crossed[j][i] = 1;
+				}
 			}
 		}
 	}
